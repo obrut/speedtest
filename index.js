d077bc4cae412d2c8bff0bbc3ee786c6ee94ffb2
Gustaf Ridderstolpe <gustaf.ridderstolpe@enfogroup.com>
2020-01-23 16:19:52 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const mqtt = require('mqtt');
+
+async function runTest() {
+	const {stdout, stderr} = await exec('speedtest-cli --json');
+	const o = JSON.parse(stdout);
+	return o;
+}
+
+function sendMQTT(ping, download, upload) {
+	console.log(ping, download, upload);
+
+	var client  = mqtt.connect('mqtt://192.168.1.72')
+ 
+	client.on('connect', function () {
+		client.subscribe('broadband', function (err) {
+			if (!err) {
+				client.publish('broadband/download', download.toString());
+				client.publish('broadband/upload', upload.toString());
+				client.publish('broadband/ping', ping.toString());
+			}
+		})
+	})
+}
+
+return runTest().then(d =>  { sendMQTT(d.ping, d.download, d.upload) });
